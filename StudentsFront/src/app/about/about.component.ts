2a486bd4f215661a144e4ba6df848e1c4ba2d813
Strzelba72 <mikolaj.strzelczyk72@outlook.com>
2024-06-03 09:45:15 +0200

    Initial commit on main

---
+++ StudentsFront/src/app/about/about.component.ts
@@ -0,0 +1,17 @@
+import { Component } from '@angular/core';
+import { OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-about',
+  templateUrl: './about.component.html',
+  styleUrl: './about.component.css'
+})
+
+export class AboutComponent implements OnInit {
+  currentDate = new Date();
+
+  constructor() { }
+
+  ngOnInit(): void {
+  }
+}
